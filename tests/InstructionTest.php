75654723fdf098c33225d052937a11830421f673
Johann Pardanaud <pardanaud.j@gmail.com>
2018-01-19 18:16:06 +0100

    Add instruction tests

---
+++ tests/InstructionTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace ExtractrIo\Rialto\Tests;
+
+use PHPUnit\Framework\TestCase;
+use ExtractrIo\Rialto\Traits\IdentifiesResource;
+use ExtractrIo\Rialto\{Instruction, ResourceIdentity};
+use ExtractrIo\Rialto\Interfaces\ShouldIdentifyResource;
+
+class InstructionTest extends TestCase
+{
+    /** @test */
+    public function call_action_builds_appropriate_json()
+    {
+        $instruction = Instruction::withCall('methodName', 'arg1', ['arg2' => 'value']);
+
+        $this->assertEquals([
+            'type' => 'call',
+            'name' => 'methodName',
+            'value' => [
+                [
+                    'type' => 'json',
+                    'value' => 'arg1',
+                ],
+                [
+                    'type' => 'json',
+                    'value' => ['arg2' => 'value'],
+                ],
+            ],
+            'catched' => false,
+        ], $instruction->jsonSerialize());
+    }
+
+    /** @test */
+    public function get_action_builds_appropriate_json()
+    {
+        $instruction = Instruction::withGet('propertyName');
+
+        $this->assertEquals([
+            'type' => 'get',
+            'name' => 'propertyName',
+            'catched' => false,
+        ], $instruction->jsonSerialize());
+    }
+
+    /** @test */
+    public function set_action_builds_appropriate_json()
+    {
+        $instruction = Instruction::withSet('propertyName', ['a' => 'b']);
+
+        $this->assertEquals([
+            'type' => 'set',
+            'name' => 'propertyName',
+            'value' => [
+                'type' => 'json',
+                'value' => ['a' => 'b'],
+            ],
+            'catched' => false,
+        ], $instruction->jsonSerialize());
+    }
+
+    /** @test */
+    public function resource_linking_builds_appropriate_json()
+    {
+        $resource = new class implements ShouldIdentifyResource {
+            use IdentifiesResource;
+        };
+
+        $id = uniqid('', true);
+        $resource->setResourceIdentity(new ResourceIdentity('Fake', $id));
+
+        $instruction = Instruction::withGet('propertyName')->linkToResource($resource);
+
+        $this->assertEquals([
+            'type' => 'get',
+            'name' => 'propertyName',
+            'resource' => $id,
+            'catched' => false,
+        ], $instruction->jsonSerialize());
+    }
+
+    /** @test */
+    public function error_catching_builds_appropriate_json()
+    {
+        $instruction = Instruction::withGet('propertyName')->shouldCatchErrors(true);
+
+        $this->assertEquals([
+            'type' => 'get',
+            'name' => 'propertyName',
+            'catched' => true,
+        ], $instruction->jsonSerialize());
+    }
+}
