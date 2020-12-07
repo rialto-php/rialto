dbd9500fce8d3e8a3c1c553f2d7048f80cdec6b9
Johann Pardanaud <pardanaud.j@gmail.com>
2020-12-07 13:47:05 +0100

    Add phpstan-phpunit analysis

--- tests/Implementation/Resources/Stats.php
+++ tests/Implementation/Resources/Stats.php
@@ -4,6 +4,10 @@ namespace Nesk\Rialto\Tests\Implementation\Resources;
 
 use Nesk\Rialto\Data\BasicResource;
 
+/**
+ * @method bool isDirectory()
+ * @method bool isFile()
+ */
 class Stats extends BasicResource
 {
     //
